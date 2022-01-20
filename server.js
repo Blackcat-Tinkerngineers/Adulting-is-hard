709851e452571667394e44b792af289643b2319d
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-01-19 19:43:27 -1000

    updates needed to untracked files

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ app.listen(3001, () => {
     console.log(`API server now on port 3001!`);
   });
 
-  const { notes } = require('/da');
+  const { notes } = require('notes');
 
   app.get('/api/notes', (req, res) => {
     res.send('Hello!');
