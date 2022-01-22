fcddd6ab434b73b1449bab2645752a8d3b7d0ac6
Chelsie L Bangloy-Gonsalves <clbgonsalves@gmail.com>
2022-01-21 23:45:11 -1000

    wrapping up server.js. Tomorrow will start on FS & DB setup

--- server.js
+++ server.js
@@ -1,11 +1,49 @@
 const express = require('express');
 const app = express();
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
-  });
+const port = process.env.PORT || 3001;
+const path = require('path');
+const router = express.Router();
+const fs = require('fs');
+const dirPath = path.join(__dirname,'/index.html', '/notes.html');
+express.static(path.join(__dirname, '/public'));
 
-  const { notes } = require('notes');
 
-  app.get('/api/notes', (req, res) => {
-    res.send('Hello!');
+app.use(express.static('public'));
+app.use(express.static('files'));
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.status(200).send({
+    date: '01-21-2022',
+    title: 'shopping list',
+    note: 'eggs'
   });
+});
+
+app.post('/notes/:id', (req, res) => {
+  const { id } = req.params;
+  const { text } = req.body;
+
+  if (!text) {
+    return res.status(404).send({ message: 'Huston we have a problem! Tracker unable to update entry request' });
+  }
+  res.send({
+    Note: 'Tracker is ready for a new entry ${text} and ID of ${id}',
+
+  });
+});
+
+
+router.get('/',function(req,res){
+  res.sendFile(path.join(public, '/index.html'));
+
+});
+
+router.get('/notes',function(req,res){
+  res.sendFile(path.join(public, '/notes.html'));
+});
+
+
+app.use('/', router);
+app.listen(port, () => console.log('listening on port ${port}...'));
+
